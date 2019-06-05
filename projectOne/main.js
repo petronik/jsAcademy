e9d25e599b9c213b4ded90bf99c22fb488531493
Petronik <petronik@ukr.net>
2019-06-06 02:05:55 +0300

    add more code

--- projectOne/main.js
+++ projectOne/main.js
@@ -1,23 +1,38 @@
-var money = prompt("Ваш бюджет на месяц?" , "1000");
-var time = prompt("Введите дату в формате YYYY-MM-DD", "2012-12-12");
-var itemOfExpenditure = prompt(
-    "Введите обязательную статью расходов в этом месяце", "qwerty"),
-itemOfExpenditureCost = prompt("Во сколько обойдется?", "123");
+var money = +prompt("Ваш бюджет на месяц?" , "");
+var time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 
 var appData = {
-    budget: `${money}`,
-    timeData: `${time}`,
-    expenses: {
-        itemOfExpenditure,
-        itemOfExpenditureCost
-    },
+    budget: money,
+    timeData: time,
+    expenses: {},
     optionalExpenses: {},
     income: [],
     savings: false
 };
-var oneDayBudget = money / 30;
 
-alert("Ваш бюджет на " + appData.timeData + " составляет " +
- Math.floor(oneDayBudget) + " гривен");
-console.log(appData);
+
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
+
+    if ( (typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null 
+        && a != '' && b != '' && a.length < 50) {
+            console.log("done!");
+        appData.expenses[a] = b;
+    } else {
+            console.log("all good!");
+    }
+    
+
+};
+appData.moneyPerDay = appData.budget / 30
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log(" Минимальный уровень достатка")
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay <2000) {
+    console.log("Средний уровень достатка")
+}
