1545d45c36dc445419116da3d3da8db829be1178
Petronik <petronik@ukr.net>
2019-06-06 22:13:53 +0300

    practice

--- projectOne/main.js
+++ projectOne/main.js
@@ -15,24 +15,28 @@ var appData = {
 
 for (let i = 0; i < 2; i++) {
     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-        b = prompt("Во сколько обойдется?", "");
+        b = +prompt("Во сколько обойдется?", "");
 
-    if ( (typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null 
-        && a != '' && b != '' && a.length < 50) {
+    if ( (typeof(a)) === "string" && (typeof(a)) != null && 
+    (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
             console.log("done!");
         appData.expenses[a] = b;
     } else {
-            console.log("all good!");
+            
     }
     
 
-};
-appData.moneyPerDay = appData.budget / 30
+}
+appData.moneyPerDay = appData.budget / 30;
 
 alert("Ежедневный бюджет: " + appData.moneyPerDay);
 
 if(appData.moneyPerDay < 100) {
-    console.log(" Минимальный уровень достатка")
+    console.log(" Минимальный уровень достатка");
 } else if (appData.moneyPerDay > 100 && appData.moneyPerDay <2000) {
-    console.log("Средний уровень достатка")
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
 }
