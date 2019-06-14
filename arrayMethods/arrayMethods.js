9312ec216d11bbdbfd26405bc22cf1c8b95b78d9
Petronik <petronik@ukr.net>
2019-06-14 23:16:15 +0300

    update

--- arrayMethods/arrayMethods.js
+++ arrayMethods/arrayMethods.js
@@ -98,20 +98,31 @@
 // console.log( a==b);
 
 
-function Calculator() {
-    this.read = function() {
-        this.a = +prompt('a?', 0);
-        this.b = +prompt('b?', 0);
-    };
+// function Calculator() {
+//     this.read = function() {
+//         this.a = +prompt('a?', 0);
+//         this.b = +prompt('b?', 0);
+//     };
 
-this.sum = function()  {
-    return this.a + this.b;
-};
-this.mul = function () {
-    return this.a * this.b;
-};
+// this.sum = function()  {
+//     return this.a + this.b;
+// };
+// this.mul = function () {
+//     return this.a * this.b;
+// };
+// }
+//  var calculator = new Calculator();
+//  calculator.read();
+//  alert( "Cymma=" + calculator.sum() );
+//  alert("Произведение=" + calculator.mul() );
+
+function Accumulator(startingValue) {
+    this.value = startingValue;
+    this.read = function() {
+        this.value += +prompt('Сколько добавлять будем?', 0);
+    };
 }
- var calculator = new Calculator();
- calculator.read();
- alert( "Cymma=" + calculator.sum() );
- alert("Произведение=" + calculator.mul() );
+ var accumulator = new Accumulator(1);
+ accumulator.read();
+ accumulator.read();
+ alert( accumulator.value);
