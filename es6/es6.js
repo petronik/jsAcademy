b99dad1289aec510ad154b3f2ceec03e1ee9202f
Petronik <petronik@ukr.net>
2019-07-24 16:28:01 +0300

    update

--- es6/es6.js
+++ es6/es6.js
@@ -101,30 +101,59 @@
 //         console.log(this);
 // };
 
-// func();
+// // func();
 
-let obj = {
-        number:5,
-        sayNumber: function() {
-                let say = () => {
-                        console.log(this);
-                };
-                say();
+// let obj = {
+//         number: 5,
+//         sayNumber: function() {
+//                 let say = () => {
+//                         console.log(this);
+//                 };
+//                 say();
+//         }
+// };
+
+// obj.sayNumber();
+
+// let btn = document.querySelector('button');
+
+// btn.addEventListener('click', function() {
+//         let show = () => {
+//                 console.log(this);
+//         };
+//         show();
+// });
+
+
+// function calcOrDouble(number, basis = 2) {
+//         // basis = basis || 2; ES5
+//         console.log(number*basis);
+// }
+// calcOrDouble(2,4);
+// calcOrDouble(4);
+
+class Rectangle {
+        constructor(height, width = 20) {
+                this.height = height;
+                this.width = width;
         }
-};
-
-obj.sayNumber();
-
-let btn = document.querySelector('button');
-
-btn.addEventListener('click', function() {
-        let show = () => {
-                console.log(this);
-        };
-        show();
-});
-
-function calcOrDouble(number, basis) {
-        basis = basis || 2;
-        console.log(number * basis);
+        calcArea() {
+                return this.height * this.width;
+        }
 }
+const square = new Rectangle(10);
+console.log(square.calcArea());
+
+let video = ['youtube', 'vimeo', 'rutube'],
+        blogs = ['worlpress', 'livejournal', 'blogger'],
+        internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+function log(a, b, c) {
+        console.log(a);
+        console.log(b);
+        console.log(c);
+        console.log(a+b+c);
+}
+let numbers = [2,5,7];
+log(...numbers);
