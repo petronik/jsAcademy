5ff72727054e0b43551835a574cba5dec7170fa2
Petronik <petronik@ukr.net>
2019-08-05 11:51:48 +0300

    updated code

--- cssFitures/js/main.js
+++ cssFitures/js/main.js
@@ -7,3 +7,47 @@ particlesJS.load('particles-js', 'js/particles.json', function() {
 
 // Card 3 >>>>>>>>
 
+let boxBg = document.getElementById('boxBg');
+
+    let xmouse, ymouse;
+
+    boxBg.addEventListener('mousemove', function(e) {
+      xmouse = e.clientX || e.pageX;
+      ymouse = e.clientY || e.pageY;
+
+      // console.log(e.pageX);
+      
+    });
+
+    let ball = document.querySelector('#ball');
+    // console.log(ball);
+    let x = void 0,
+        y = void 0,
+        dx = void 0,
+        dy = void 0,
+        tx = 0,
+        ty = 0,
+        key = -1;
+
+    let followMouse = function followMouse() {
+      requestAnimationFrame(followMouse);
+
+      if(!x || !y) {
+        x = xmouse;
+        y = ymouse;
+      } else {
+        dx = (xmouse - x) * 0.125;
+        dy = (ymouse - y) * 0.125;
+        if(Math.abs(dx) + Math.abs(dy) < 0.1) {
+          x = xmouse;
+          y = ymouse;
+        } else {
+          x += dx;
+          y += dy;
+        }
+      }
+      console.log(Math.abs(dx));
+      ball.style.left = x +'px';
+      ball.style.top = y +'px';
+    };
+    followMouse();
